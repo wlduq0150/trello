ae4ec30859a4243a10d89af4ed34b8e9cac81f18
데브티브 <thive8564@gmail.com>
2024-01-06 01:04:00 +0900

    add comment/controller, service

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,7 +1,28 @@
 import { Controller } from "@nestjs/common";
 import { CommentService } from "./comment.service";
+import { UseGuards } from "@nestjs/common";
+import { accessTokenGuard } from "src/auth/guard/access-token.guard";
+import { Get } from "@nestjs/common";
+import { Param } from "@nestjs/common";
+import { Post } from "@nestjs/common";
+import { CreateCommentDto } from "./dto/create-comment.dto";
+import { User } from "src/entity/user.entity";
+import { UserId } from "src/auth/decorators/userId.decorator";
 
 @Controller("comment")
 export class CommentController {
     constructor(private readonly commentService: CommentService) {}
+
+    @UseGuards(accessTokenGuard)
+    @Get("cards/:cardid/comments")
+    async commentfind(@Param('cardid') cardid: number) {
+        return await this.commentService.commentfind(cardid);
+    }
+
+
+    @UseGuards(accessTokenGuard)
+    @Post("cards/:cardid/comments")
+    async comment(createComment: CreateCommentDto, @Param('cardid') cardid: number, @UserId user: User) {
+        return await this.commentService.comment(createComment, user, cardid)
+    }
 }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,51 @@
+import { BadRequestException } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Card } from 'src/entity/card.entity';
+import { Comment } from 'src/entity/comment.entity';
+import { Repository } from 'typeorm';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { User } from 'src/entity/user.entity';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+    constructor(
+        @InjectRepository(Comment)
+        private commentRepository: Repository<Comment>,
+        @InjectRepository(Card)
+        private cardRepository: Repository<Card>
+    ) {}
+// 댓글 기능 조회 (카드 id로 불러오기위해서 entitiy 수정 확인)
+    async commentfind(cardid: number) {
+        const comment = await this.commentRepository.find({
+            // where : {cardid},
+            
+        })
+    }
+
+    // 댓글 작성
+    async comment(createCommentDto: CreateCommentDto, user: User, cardid: number) {
+        const existCard = await this.cardRepository.findOne( { where:{id: cardid}})
+
+        if(!existCard) {
+            throw new BadRequestException('카드를 확인해주세요');
+        }
+
+
+        const comment = new Comment();
+        comment.content = createCommentDto.comment;
+        comment.card = existCard;
+        comment.user = user;
+
+        await this.commentRepository.save(comment);
+
+        return comment.content;
+    }
+
+
+    // 댓글 수정
+    async commentUpdate(cardid: number, commentid: number) {
+        
+    }
+    
+}
